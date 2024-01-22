8e365b3269ed7e056bca999bc20b0eed7c381668
Ilef01 <bchinyyilef@gmail.com>
2024-01-22 13:13:29 -0800

    handling the case where x is bigger than the length of the list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
+    j = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
+            j = j + 1
+        print()
     except IndexError:
         pass
+    return (j)
