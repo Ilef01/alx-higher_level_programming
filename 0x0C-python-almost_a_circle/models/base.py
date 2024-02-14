77d4cbf256a643eb7350d21ab1686433419e4c55
Ilef01 <bchinyyilef@gmail.com>
2024-02-14 13:35:43 -0800

    documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 class Base:
+    """
+    Base class for object creation.
+
+    Attributes:
+        __nb_onjects (int): Private class variable to keep trach of the number of objects created
+        id (int): Instance variable representing the identifier of an object.
+    Methods:
+        __init__(self, id=None): Constructor method for initializing the class objects.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Constructor method
+
+        Args:
+            id (int, optional): Identifier for the object, if not provided a unique Id will be assigned
+        """
         super().__init__()
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = Base.__nb_objectm
