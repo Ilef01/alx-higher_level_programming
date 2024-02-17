7e9dc751c6228799dd0db7885e5db551832f8a1a
Ilef01 <bchinyyilef@gmail.com>
2024-02-17 06:37:22 -0800

    pycodestyle fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """Defines a base class for object creation with id assignment"""
+
+
 class Base:
     """
     Base class for object creation.
-
-    Attributes:
-        __nb_onjects (int): Private class variable to keep trach of the number of objects created
-        id (int): Instance variable representing the identifier of an object.
-    Methods:
-        __init__(self, id=None): Constructor method for initializing the class objects.
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        Constructor method
-
-        Args:
-            id (int, optional): Identifier for the object, if not provided a unique id will be assigned
+        Constructor assigning an id for the object
         """
         if id is not None:
             self.id = id
