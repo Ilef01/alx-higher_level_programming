ac1005a5dc42605ebbdf510a1f33fe1fcc2c87db
Ilef01 <bchinyyilef@gmail.com>
2024-02-18 11:12:23 -0800

    Getters and setters are now defined directly in the class  (not inside the __init__ method)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 """Defines a class for rectangle creation"""
 
 
-class Recatangle(Base):
+class Rectangle(Base):
     """
     Rectangle class inherits from Base
     """
@@ -16,34 +16,34 @@ class Recatangle(Base):
         self.__x = x
         self.__y = y
 
-        def width(self):
-            """Getter for retrieving width"""
-            return self.__width
+    def width(self):
+        """Getter for retrieving width"""
+        return self.__width
 
-        def width(self, value):
-            """Setter for setting width"""
-            self.__width = value
+    def width(self, value):
+        """Setter for setting width"""
+        self.__width = value
 
-        def heigth(self):
-            """Getter for retrieving height"""
-            return self.__height
+    def heigth(self):
+        """Getter for retrieving height"""
+        return self.__height
 
-        def height(self, value):
-            """Setter for setting height"""
-            self.__height = value
+    def height(self, value):
+        """Setter for setting height"""
+        self.__height = value
 
-        def width(self):
-            """Getter for retrieving the x coordinate"""
-            return self.__x
+    def width(self):
+        """Getter for retrieving the x coordinate"""
+        return self.__x
 
-        def width(self, value):
-            """Setter for setting x"""
-            self.__x = value
+    def width(self, value):
+        """Setter for setting x"""
+        self.__x = value
 
-        def width(self):
-            """Getter for retrieving the y coordinate"""
-            return self.__y
+    def width(self):
+        """Getter for retrieving the y coordinate"""
+        return self.__y
 
-        def width(self, value):
-            """Setter for setting y"""
-            self.__y = value
+    def width(self, value):
+        """Setter for setting y"""
+        self.__y = value
