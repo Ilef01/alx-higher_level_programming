fbc33ef675b96b8524a7940424503fce49a077f8
Ilef01 <bchinyyilef@gmail.com>
2024-04-08 15:11:25 -0700

    semistandards checked

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const {argv} = process;
-if (argv.length <= 2)
-	console.log('No argument');
-else if (argv.length === 3)
-	console.log('Argument found');
-else
-	console.log('Arguments found');
+const { argv } = process;
+if (argv.length <= 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
