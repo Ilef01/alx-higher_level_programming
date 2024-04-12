7405deb807bc02e6ed900693f892418a1046e9b1
Ilef01 <bchinyyilef@gmail.com>
2024-04-12 14:08:24 -0700

    documentation added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""lists states starting with N from a MYSQL database using MySQLdb"""
+""" a script that lists all states with a name starting with N from
+                   the database hbtn_0e_0_usai """
 
 if __name__ == '__main__':
     import MySQLdb
