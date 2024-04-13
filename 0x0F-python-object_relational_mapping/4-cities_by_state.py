d37a46a39d8d61045e8b2b467e0f4045d896fe37
Ilef01 <bchinyyilef@gmail.com>
2024-04-13 06:10:37 -0700

    adding order

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,8 @@ if __name__ == '__main__':
     cur.execute(
             "SELECT cities.state_id, cities.name, states.name "
             "FROM cities "
-            "JOIN states ON cities.state_id = states.id"
+            "JOIN states ON cities.state_id = states.id "
+            "ORDER BY cities.id"
     )
 
     """ Obtaining query results """
