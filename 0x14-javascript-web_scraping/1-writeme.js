320d8062c89998aca411639f656fe1b0c644070f
Ilef01 <bchinyyilef@gmail.com>
2024-05-22 15:36:25 -0700

    conversion to synchronous file wrinting

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,19 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-if (!file) {
-  console.error('No file provided');
-  process.exit(1);
+try {
+  fs.writeFileSync(file, string, 'utf-8');
+} catch (error) {
+  console.error('Error writing to file: ', error);
+  process.exit(1);;
 }
-
-if (!string) {
-  console.error('No string provided');
-  process.exit(1);
-}
-
-fs.writeFile(file, string, 'utf-8', function (error) {
-  if (error) {
-    console.error('Error writing to file: ', error);
-    process.exit(1);
-  }
-});
