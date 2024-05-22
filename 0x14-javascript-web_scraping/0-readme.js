5974c404570cf482bf5dde1d0de7a3fcab1a6d17
Ilef01 <bchinyyilef@gmail.com>
2024-05-22 05:43:23 -0700

    console.log fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,12 +5,11 @@ const file = process.argv[2];
 
 if (!file) {
   console.error('No file provided');
-  process.exit(1);
 }
 fs.readFile(file, 'utf-8', function (error, data) {
   if (error) {
     console.error('error:', error);
     process.exit(1);
   }
-  console.log('body', data);
+  console.log(data);
 });
